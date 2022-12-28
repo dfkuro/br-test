f49a4587e167e5321f0f84667eacbcdc368827a4
dfkuro <izmirreffi@gmail.com>
2022-12-28 12:24:06 -0600

    This is the first commit

---
+++ hosting/pages/about.tsx
@@ -0,0 +1,8 @@
+export default function AboutPage() {
+	return (
+		<div>
+			<h2>This is the about page</h2>
+			<p>Welcome to the about info</p>
+		</div>
+	);
+}
---
+++ hosting/pages/index.tsx
@@ -0,0 +1,19 @@
+import Link from 'next/link'
+import Head from 'next/head';
+
+export default function IndexPage() {
+	return (
+		<div>
+			<Head>
+				<title>Surman core</title>
+				<meta name="viewport" content="initial-scale=1, width=device-width" />
+			</Head>
+			<div>
+				<h1>
+					Hello World.
+				</h1>
+				<Link href="/about">About</Link>
+			</div>
+		</div>
+	)
+}
